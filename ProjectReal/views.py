1c07a3d79f1b67de9939cc1986a79fe9ea5471ac
Apurv Chatur <chaturapurv@gmail.com>
2021-09-28 03:14:34 +0530

    Project Model Added

--- ProjectReal/views.py
+++ ProjectReal/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from ProjectDesign.models import (Home,
+                                  Product,
+                                  )
 
 def home_view(request):
+    home = Home.objects.all()
+    products = Product.objects.all()
+
     template_name = "ProjectReal/home-page.html"
-    context = {}
+    context = {
+        'home': home,
+        'products': products,
+
+    }
     return render(request, template_name, context)
